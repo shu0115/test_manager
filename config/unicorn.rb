bf8bb2a8917b65430231a3c5f446080899f73975
Shun Matsumoto <shun.matsumoto@pixta.co.jp>
2012-02-23 17:33:58 +0900

    unicorn.rb修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,4 +4,4 @@
 worker_processes 2
  
 # ソケットパス
-listen "#{File.expand_path('../tmp/sockets', __FILE__)}/test_manager.sock"
+listen "#{File.expand_path('../../tmp/sockets', __FILE__)}/test_manager.sock"
