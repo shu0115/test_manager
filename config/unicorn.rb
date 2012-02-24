c871ad74e34d929dd5ba7c36445aeba81f210f52
Shun Matsumoto <shun.matsumoto@pixta.co.jp>
2012-02-24 11:43:53 +0900

    ・PID設定追加

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,3 +5,6 @@ worker_processes 2
  
 # ソケットパス
 listen "#{File.expand_path('../../tmp/sockets', __FILE__)}/test_manager.sock"
+
+# PID
+pid "#{File.expand_path('../../tmp/pids', __FILE__)}/unicorn.pid"
