80d90dc2b94696705d5881e9782d4da298380df4
Shun Matsumoto <shun.matsumoto@pixta.co.jp>
2012-02-24 13:43:40 +0900

    Unicorn設定修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,9 +2,19 @@
 
 # ワーカー数
 worker_processes 2
- 
+
+# ワーキングディレクトリ
+working_directory File.expand_path('../../', __FILE__)
+
 # ソケットパス
 listen "#{File.expand_path('../../tmp/sockets', __FILE__)}/test_manager.sock"
 
-# PID
+# プロセスID
 pid "#{File.expand_path('../../tmp/pids', __FILE__)}/unicorn.pid"
+
+# タイムアウト
+timeout 10
+
+# ログ
+stdout_path "#{File.expand_path('../../log', __FILE__)}/unicorn_stdout.log"
+stdout_path "#{File.expand_path('../../log', __FILE__)}/unicorn_stderr.log"
