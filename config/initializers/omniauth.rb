733e53f210b57c6dc7466f45b6e4084fb3706526
Shun Matsumoto <shun.matsumoto@pixta.co.jp>
2012-02-27 14:24:51 +0900

    auth設定修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :github, GITHUB_KEY, GITHUB_SECRET
-  provider :twitter, TWITTER_KEY, TWITTER_SECRET
+#  provider :twitter, TWITTER_KEY, TWITTER_SECRET
 end
